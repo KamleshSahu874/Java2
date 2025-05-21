3eeda745eb5ec679f05d97d2e13a36836bc4168d
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-21 12:40:53 +0530

    Initial Commit

---
+++ src/Compress.java
@@ -0,0 +1,17 @@
+public class Compress {
+    public static void main(String[] args) {
+        String str="aaaabbbcccc";
+        StringBuilder newStr=new StringBuilder();
+        int count=1;
+        for (int i=1;i<str.length();i++){
+            if(str.charAt(i)==str.charAt(i-1)){
+                count++;
+            }
+            else{
+               newStr.append(str.charAt(i+1)).append(count);
+               count=1;
+            }
+        }
+        System.out.println(newStr);
+    }
+}
