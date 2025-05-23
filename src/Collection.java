add2f867658e0b336a5a8b0df8d0aadabe0f79e9
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-23 10:27:54 +0530

    Initial Commit

--- src/Collection.java
+++ src/Collection.java
@@ -6,6 +6,13 @@ public class Collection {
         arr.add("Kamlesh");
         arr.add("Sahu");
         System.out.println(arr);
+
+        //System.out.println(arr.get(1));
+        arr.set(2, "kamlesh");
+        arr.remove(2);
+        System.out.println(arr);
+        System.out.println(arr.size());
+
     }
 
 }
