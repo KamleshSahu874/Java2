819798ef29d26162986e39a2726cb2ca6a2d9852
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-23 10:41:36 +0530

    Initial Commit

---
+++ src/Collections1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Collections1 {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add("a");
+        arr.add("k");
+        arr.add("c");
+        arr.add("M");
+        arr.add("J");
+        Collections.sort(arr);
+        System.out.println(arr);
+
+
+
+
+
+    }
+
+}
+
