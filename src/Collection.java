ee8b86942e002823498179997fb4730cf61f6c8e
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-23 10:15:03 +0530

    Initial Commit

---
+++ src/Collection.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Collection {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Kamlesh");
+        arr.add("Sahu");
+        System.out.println(arr);
+    }
+
+}
