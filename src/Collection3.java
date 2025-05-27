4442ad8b9200d1733152c5cf4280a8622cabc7ca
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-27 11:33:46 +0530

    Initial Commit

---
+++ src/Collection3.java
@@ -0,0 +1,16 @@
+
+import java.util.TreeSet;
+
+public class Collection3 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 3, 4, 4, 5};
+        TreeSet<Integer> uniqueSet = new TreeSet<>();
+        for (int i : arr) {
+            if(uniqueSet.contains(i)) {
+                System.out.println("Duplicate found: " + i);
+            } else {
+                uniqueSet.add(i);
+            }
+        }
+    }
+}
