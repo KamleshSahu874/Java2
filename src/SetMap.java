d5db9985845e5d17eee47396a72e7b0e07a998cb
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-27 12:34:17 +0530

    Initial Commit

---
+++ src/SetMap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class SetMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("apple", 1);
+        map.put("banana", 2);
+        map.put("orange", 3);
+        System.out.println(map.get("grapes"));
+    }
+
+}
