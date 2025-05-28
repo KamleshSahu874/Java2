98e18ec321dadac185aae4e14fdb72468688063d
kamlesh <sahukamleshkumar404@gmail.com>
2025-05-28 14:58:16 +0530

    Initial Commit

---
+++ src/CharacterCount.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class CharacterCount {
+    public static void main(String[] args) {
+        String str = "programming";
+        char[] ch = str.toCharArray();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char i : ch) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        System.out.println("Character count in the string:");
+        for (char c : map.keySet()) {
+            System.out.println(c + ": " + map.get(c));
+        }
+    }
+}
+
