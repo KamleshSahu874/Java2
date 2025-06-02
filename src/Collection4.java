fa8e93e4d9466b4e34b108f44d20f7c4a2767562
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-02 11:01:14 +0530

    Initial Commit

---
+++ src/Collection4.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Collection4 {
+   public static void main(String[] args) {
+       ArrayList<Integer> list=new ArrayList<>();
+       list.add(1);
+       list. add(0);
+       list.add(4);
+       Collections.sort(list);
+       System.out.println(list);
+
+
+    }
+}
