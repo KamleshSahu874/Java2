ab78b1ccdf10baca792665ba8303dd1004707896
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-02 10:45:36 +0530

    Initial Commit

--- src/Student1.java
+++ src/Student1.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Student1 {
-    public static void main(String[] args) {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(1);
-        list. add(0);
-        list.add(4);
-        Collections.sort(list);
-        System.out.println(list);
+   public static void main(String[] args) {
+       ArrayList<Integer> list=new ArrayList<>();
+       list.add(1);
+       list. add(0);
+       list.add(4);
+       Collections.sort(list);
+       System.out.println(list);
 
 
     }
