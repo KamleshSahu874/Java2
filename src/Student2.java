4ae32bb0f01ca7d865bbc96d6b54a2a09b321f37
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-02 10:45:57 +0530

    Initial Commit

---
+++ src/Student2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student implements Comparable<Student> {
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+    public String toString() {
+        return age + " " + name;
+    }
+
+    public int compareTo(Student s) {
+        return this.age - s.age; // Ascending order
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+
+        list.add(new Student(22, "Akbar"));
+        list.add(new Student(19, "Birbal"));
+        list.add(new Student(21, "Tansen"));
+
+        Collections.sort(list); // Uses compareTo
+
+        for (Student s : list) {
+            System.out.println(s);
+        }
+    }
+}
