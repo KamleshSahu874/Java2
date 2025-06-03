e3cf12e6e6a6e37b3ee795c3ad6fb857e0da495a
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-03 10:40:20 +0530

    Initial Commit

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -3,13 +3,18 @@
 class MyThread extends Thread{
     public void run(){
         System.out.println("hello");
+        try{
+            Thread.sleep(2000);
+        }catch (Exception e){
+            System.out.println("hiii");
+        }
     }
 }
 class Multithreading{
     public static void main(String[] args) {
-        MyThread t1=new MyThread();
-        t1.start();
-        System.out.println("hiii");
+       MyThread t1=new MyThread();
+       t1.start();
+       System.out.println("hiii");
 
 
 
