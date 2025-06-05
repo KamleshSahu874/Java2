c825973cc1b57f952f016c557de29e8569003c7c
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-05 12:46:31 +0530

    Initial Commit

---
+++ src/Demo3.java
@@ -0,0 +1,14 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        System.out.println("idhar dekho");
+        Thread t1=new Thread(()->{
+            System.out.println("hello bro!");
+        });
+        Thread t2=new Thread(()->{
+            System.out.println("hiii");
+
+        });
+        t1.start();
+        t2.start();
+    }
+}
