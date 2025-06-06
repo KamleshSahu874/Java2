f1da63cd12b8261d541c6a30f70c77a5e6539e11
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-06 10:42:03 +0530

    Initial Commit

---
+++ src/Filter.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+import java.util.stream.*;
+//import java.lang.reflect.Array;
+
+public class Filter {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer>evenList=list.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(evenList);
+    }
+
+}
+
