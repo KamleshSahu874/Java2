0119d040706ea9e49355ff1b485453868a9e55c6
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-07 12:10:32 +0530

    Initial Commit

--- src/Filter.java
+++ src/Filter.java
@@ -9,7 +9,7 @@ public class Filter {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<Integer>evenList=list.stream()
                 .map(n->n*5)
-               // .filter(n -> n % 2 == 0)
+                .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
 
         System.out.println(evenList);
