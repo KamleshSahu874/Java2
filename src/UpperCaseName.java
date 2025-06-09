c7885f19921c5a605b365c576d42aec1d9d17d31
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-09 10:19:26 +0530

    Initial Commit

--- src/UpperCaseName.java
+++ src/UpperCaseName.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 public class UpperCaseName {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Ram", "Sita", "Krishna", "Radha", "Mohan", "Ali");
+        List<String> names = Arrays.asList("Ram", "Sita", "Krishna", "Radha", "Mohan", "Om");
 
         List<String> filteredNames = names.stream()
                 .filter(name -> name.length() > 4)
