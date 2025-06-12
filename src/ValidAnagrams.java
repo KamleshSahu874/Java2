c72a64e9dcb24667bcbc4b5460ec5c764f3ca7e4
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-12 10:17:21 +0530

    Initial Commit

--- src/ValidAnagrams.java
+++ src/ValidAnagrams.java
@@ -8,19 +8,18 @@ public class ValidAnagrams {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String word : words) {
-            // Convert word to character array, sort it, then make a string again
+
             char[] chars = word.toCharArray();
             Arrays.sort(chars);
             String sorted = new String(chars);
 
-            // Put into map
+
             if (!map.containsKey(sorted)) {
                 map.put(sorted, new ArrayList<>());
             }
             map.get(sorted).add(word);
         }
 
-        // Print anagram groups
         for (List<String> group : map.values()) {
             if (group.size() > 1) {
                 System.out.println(group);
