b95cf979ceda0a60151463d447ad8a43fdde1b70
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-14 22:20:44 +0530

    Initial Commit

--- src/MaxMinArray.java
+++ src/MaxMinArray.java
@@ -10,7 +10,6 @@ public class MaxMinArray {
 
         int[] arr = new int[n];
 
-
         System.out.println("Enter the elements of the array:");
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
