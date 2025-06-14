6f957ab6bd478d3100c5283ee117decb80a4861d
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-14 22:20:27 +0530

    Initial Commit

--- src/MaxMinArray.java
+++ src/MaxMinArray.java
@@ -4,13 +4,13 @@ public class MaxMinArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input array size
+
         System.out.print("Enter the size of the array: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
-        // Input array elements
+
         System.out.println("Enter the elements of the array:");
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -19,7 +19,7 @@ public class MaxMinArray {
         int max = arr[0];
         int min = arr[0];
 
-        // Find max and min
+
         for(int i = 1; i < n; i++) {
             if(arr[i] > max)
                 max = arr[i];
