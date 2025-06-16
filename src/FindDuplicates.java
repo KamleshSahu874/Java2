b22d38b9d9b29ffb5b1e907f5ccc67937fe3a937
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-16 12:57:48 +0530

    Initial Commit

--- src/FindDuplicates.java
+++ src/FindDuplicates.java
@@ -4,12 +4,11 @@ public class FindDuplicates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input array size
+
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        // Input array elements
         System.out.println("Enter array elements:");
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
