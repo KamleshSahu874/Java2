b6245feeb16b0ec3df33a78f740ce1295e1fc200
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-20 12:19:58 +0530

    Initial Commit

---
+++ src/PairSumFinder.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class PairSumFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input: size of array
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input: array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Input: target sum
+        int target = sc.nextInt();
+
+        // Check pairs where i < j
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                }
+            }
+        }
+    }
+}
