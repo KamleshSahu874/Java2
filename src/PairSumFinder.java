ac0d7750ee41a838f89b7f3debce8bdf4d324b53
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-20 12:20:48 +0530

    Initial Commit

--- src/PairSumFinder.java
+++ src/PairSumFinder.java
@@ -4,16 +4,13 @@ public class PairSumFinder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input: size of array
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        // Input: array elements
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        // Input: target sum
         int target = sc.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
