10c2977911ec872b4c8008c44b99d701ed0ae3de
kamlesh <sahukamleshkumar404@gmail.com>
2025-06-30 13:05:53 +0530

    Initial Commit

---
+++ src/Intersection.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Intersection {
+    public static List<Integer> findIntersection(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        int n = arr1.length, m = arr2.length;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (i < n && j < m) {
+            // Skip duplicates in arr1
+            while (i > 0 && i < n && arr1[i] == arr1[i - 1]) i++;
+            // Skip duplicates in arr2
+            while (j > 0 && j < m && arr2[j] == arr2[j - 1]) j++;
+
+            if (i < n && j < m) {
+                if (arr1[i] == arr2[j]) {
+                    result.add(arr1[i]);
+                    i++;
+                    j++;
+                } else if (arr1[i] < arr2[j]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 2, 3, 4, 5};
+        int[] arr2 = {2, 2, 3, 5, 6};
+
+        List<Integer> intersection = findIntersection(arr1, arr2);
+
+        System.out.println("Intersection: " + intersection);
+    }
+}
