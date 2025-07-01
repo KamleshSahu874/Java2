5e0507a053ca142e72f30351c65a8b28e6b6c79f
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-01 17:45:04 +0530

    Initial Commit

---
+++ src/LeadersInArray.java
@@ -0,0 +1,19 @@
+public class LeadersInArray {
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+
+        System.out.print("Leaders: ");
+
+        int maxFromRight = arr[n - 1];
+        System.out.print(maxFromRight + " "); // Rightmost element is always a leader
+
+        // Traverse from second last to first
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxFromRight) {
+                maxFromRight = arr[i];
+                System.out.print(maxFromRight + " ");
+            }
+        }
+    }
+}
