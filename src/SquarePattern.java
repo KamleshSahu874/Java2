b623b6ab6b0587e4bb60765abe41cc56f5630964
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-03 19:13:57 +0530

    Initial Commit

---
+++ src/SquarePattern.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the square: ");
+        int n = sc.nextInt();
+
+        // Outer loop for rows
+        for (int i = 0; i < n; i++) {
+            // Inner loop for columns
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+
+        sc.close();
+    }
+}
