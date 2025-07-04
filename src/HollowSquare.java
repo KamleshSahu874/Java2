e828a4151def0c2bee90916fc5e08e1674ff6567
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-04 18:13:55 +0530

    Initial Commit

--- src/HollowSquare.java
+++ src/HollowSquare.java
@@ -8,9 +8,9 @@ public class HollowSquare {
         System.out.print("Enter the size of the square: ");
         int n = sc.nextInt();
 
-        // Loop through rows
+
         for (int i = 0; i < n; i++) {
-            // Loop through columns
+
             for (int j = 0; j < n; j++) {
                 // Print * for first/last row or first/last column
                 if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
