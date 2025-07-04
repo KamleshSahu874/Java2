f5575721acde90a6ee8a896d35b779890980bead
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-04 18:14:25 +0530

    Initial Commit

--- src/HollowSquare.java
+++ src/HollowSquare.java
@@ -12,14 +12,13 @@ public class HollowSquare {
         for (int i = 0; i < n; i++) {
 
             for (int j = 0; j < n; j++) {
-                // Print * for first/last row or first/last column
                 if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
-            System.out.println(); // Move to next row
+            System.out.println();
         }
 
         sc.close();
