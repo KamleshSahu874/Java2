18454f90e8d102ec5c7294b1773675af9aff9859
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-05 13:16:25 +0530

    Initial Commit

---
+++ src/HollowPyramid.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HollowPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars and spaces in row
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                // Print * at borders or on last row
+                if (j == 1 || j == 2 * i - 1 || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
