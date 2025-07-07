4293acf0a3dcdadea5b20a8eefda1977821353e8
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-07 12:45:09 +0530

    Initial Commit

--- src/SandglassPattern.java
+++ src/SandglassPattern.java
@@ -7,14 +7,10 @@ public class SandglassPattern {
         System.out.print("Enter number of rows (half height): ");
         int n = sc.nextInt();
 
-        // Inverted Hollow Pyramid
         for (int i = 0; i < n; i++) {
-            // Leading spaces
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-
-            // Stars and spaces
             for (int j = 0; j < 2 * (n - i) - 1; j++) {
                 if (j == 0 || j == 2 * (n - i) - 2 || i == 0) {
                     System.out.print("*");
