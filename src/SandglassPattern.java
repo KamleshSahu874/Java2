9485144d0aa3ad8a14a1e2738f238cf38969e312
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-08 11:32:45 +0530

    Initial Commit

--- src/SandglassPattern.java
+++ src/SandglassPattern.java
@@ -25,7 +25,6 @@ public class SandglassPattern {
             for (int j = 0; j < n - i ; j++) {
                 System.out.print(" ");
             }
-
             for (int j = 0; j < 2 * i + 1; j++) {
                 if (j == 0 || j == 2 * i || i == n - 1) {
                     System.out.print("*");
