2e4900a65c44085fb06a60c840133d216fd9d57a
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-09 10:19:40 +0530

    Initial Commit

---
+++ src/FilledSandglass.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class FilledSandglass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows (half height): ");
+        int n = sc.nextInt();
+
+        // Upper half (Inverted Pyramid)
+        for (int i = 0; i < n; i++) {
+            // Print leading spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            int start = i + 1;
+            int end = 2 * (n - i) - 1;
+
+            // Print numbers
+            for (int j = 0; j < end; j++) {
+                System.out.print(start + j);
+            }
+
+            System.out.println();
+        }
+
+        // Lower half (Normal Pyramid)
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            int start = n - i + 1;
+            int end = 2 * i - 1;
+
+            // Print numbers
+            for (int j = 0; j < end; j++) {
+                System.out.print(start + j);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
