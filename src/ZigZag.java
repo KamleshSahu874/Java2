853603979548eb33b96b5acf48a9081ccfa53d24
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-12 12:06:39 +0530

    Initial Commit

---
+++ src/ZigZag.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ZigZag {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of columns: ");
+        int n = sc.nextInt();
+
+        // Only 3 rows for Zigzag
+        for (int row = 1; row <= 3; row++) {
+            for (int col = 1; col <= n; col++) {
+                if ((row + col) % 4 == 0 || (row == 2 && col % 4 == 0)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
