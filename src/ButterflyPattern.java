bcf42b514e64938eba1a43c44b58ffbd899e0aa5
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-15 18:40:47 +0530

    Initial Commit

--- src/ButterflyPattern.java
+++ src/ButterflyPattern.java
@@ -9,7 +9,6 @@ public class ButterflyPattern {
 
 
         for (int i = 1; i <= n; i++) {
-            // Left stars
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
