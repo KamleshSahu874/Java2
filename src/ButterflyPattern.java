7b26c3f7a055b9cbd3ae9575b03019017e12ef6f
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-18 12:44:57 +0530

    Initial Commit

--- src/ButterflyPattern.java
+++ src/ButterflyPattern.java
@@ -16,7 +16,6 @@ public class ButterflyPattern {
             for (int j = 1; j <= 2 * (n - i); j++) {
                 System.out.print(" ");
             }
-
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
