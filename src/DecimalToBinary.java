381f3d749ac84f31acdad5121d31951965e1319f
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-19 18:52:28 +0530

    Initial Commit

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+
+        String binary = "";
+
+        int n = decimal;
+        while (n > 0) {
+            int remainder = n % 2;
+            binary = remainder + binary; // prepend remainder
+            n = n / 2;
+        }
+
+        System.out.println("Binary representation of " + decimal + " is: " + binary);
+
+        sc.close();
+    }
+}
