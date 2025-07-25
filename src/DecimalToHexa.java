7096c9d8d69d798fe0efb754525004300adc0ffe
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-25 12:13:59 +0530

    Initial Commit

--- src/DecimalToHexa.java
+++ src/DecimalToHexa.java
@@ -7,7 +7,6 @@ public class DecimalToHexa {
         System.out.print("Enter a decimal number: ");
         int decimal = sc.nextInt();
 
-
         String hex = "";
         char[] hexChars = {'0', '1', '2', '3', '4', '5', '6', '7',
                 '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
