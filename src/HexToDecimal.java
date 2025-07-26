21253ca0a8995130a01ac106cef31da7f8798d6d
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-26 11:02:56 +0530

    Initial Commit

---
+++ src/HexToDecimal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HexToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter hexadecimal number: ");
+        String hex = sc.nextLine();
+
+        // Convert to decimal
+        int decimal = Integer.parseInt(hex, 16);
+
+        System.out.println("Decimal of " + hex + " is: " + decimal);
+
+        sc.close();
+    }
+}
