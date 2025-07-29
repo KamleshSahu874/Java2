2e9ac39af9445dfa366bf9418ecf783067a02937
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-29 19:03:57 +0530

    Initial Commit

--- src/HexToDecimal.java
+++ src/HexToDecimal.java
@@ -6,6 +6,7 @@ public class HexToDecimal {
 
         System.out.print("Enter hexadecimal number: ");
         String hex = sc.nextLine();
+
         int decimal = Integer.parseInt(hex, 16);
 
         System.out.println("Decimal of " + hex + " is: " + decimal);
