7ce1873f917a1bddc67800a982b54942d6f7cb80
kamlesh <sahukamleshkumar404@gmail.com>
2025-07-30 11:39:58 +0530

    Initial Commit

---
+++ src/AnimateName.java
@@ -0,0 +1,12 @@
+public class AnimateName {
+    public static void main(String[] args) throws InterruptedException {
+        String name = "Kamlesh Kumar Sahu";
+
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(i));
+            Thread.sleep(200); // delay in milliseconds (200ms = 0.2 sec)
+        }
+
+        System.out.println(); // move to next line after animation
+    }
+}
