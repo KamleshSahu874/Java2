37745633351dc32e5077aafb2c74d6ce2af3a793
kamlesh <sahukamleshkumar404@gmail.com>
2025-08-05 10:33:11 +0530

    Initial Commit

---
+++ src/BlinkingName.java
@@ -0,0 +1,16 @@
+public class BlinkingName {
+    public static void main(String[] args) throws InterruptedException {
+        String name = "Kamlesh Kumar Sahu";
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\r" + name); // print the name
+            Thread.sleep(400);             // wait 400ms
+
+            System.out.print("\r" + " ".repeat(name.length())); // clear the name
+            Thread.sleep(400);             // wait 400ms
+        }
+
+        // Finally show the name permanently
+        System.out.print("\r" + name);
+    }
+}
